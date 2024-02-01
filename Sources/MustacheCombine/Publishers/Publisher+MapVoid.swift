6eb0e73d10ccb2ca3eeeb16953a78b2241d06fa8
Tommy Hinrichsen <th@mustache.dk>
2024-02-01 14:56:40 +0100

    Added mapvoid

---
+++ Sources/MustacheCombine/Publishers/Publisher+MapVoid.swift
@@ -0,0 +1,26 @@
+
+import Foundation
+
+import Combine
+
+@available(iOS 13.0, *)
+public extension Publisher {
+    /// Replace each upstream value with a constant.
+    ///
+    /// - Parameter value: The constant with which to replace each upstream value.
+    /// - Returns: A new publisher wrapping the upstream, but with output type `Value`.
+    func mapTo<Value>(_ value: Value) -> AnyPublisher<Value, Self.Failure> {
+        return self
+            .map { _ in value }
+            .eraseToAnyPublisher()
+    }
+    
+    /// Replace each upstream value with Void.
+    ///
+    /// - Returns: A new publisher wrapping the upstream and replacing each element with Void.
+    func mapVoid() -> AnyPublisher<Void, Self.Failure> {
+        return self
+            .map { _ in () }
+            .eraseToAnyPublisher()
+    }
+}
