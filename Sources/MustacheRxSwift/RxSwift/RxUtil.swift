e6d35d27651db08f132e6f3b95da9014b02e2eb4
Tommy Sadiq Hinrichsen <58849846+th-mustache-dk@users.noreply.github.com>
2024-02-01 10:31:33 +0100

    Update RxUtil.swift

--- Sources/MustacheRxSwift/RxSwift/RxUtil.swift
+++ Sources/MustacheRxSwift/RxSwift/RxUtil.swift
@@ -2,53 +2,6 @@ import RxSwift
 import RxCocoa
 import Foundation
 
-// workaround for Swift compiler bug, cheers compiler team :)
-public func castOptionalOrFatalError<T>(_ value: Any?) -> T? {
-    if value == nil {
-        return nil
-    }
-    let v: T = castOrFatalError(value)
-    return v
-}
-
-public func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
-    guard let returnValue = object as? T else {
-        throw RxCocoaError.castingError(object: object, targetType: resultType)
-    }
-
-    return returnValue
-}
-
-public func castOptionalOrThrow<T>(_ resultType: T.Type, _ object: AnyObject) throws -> T? {
-    if NSNull().isEqual(object) {
-        return nil
-    }
-
-    guard let returnValue = object as? T else {
-        throw RxCocoaError.castingError(object: object, targetType: resultType)
-    }
-
-    return returnValue
-}
-
-public func castOrFatalError<T>(_ value: AnyObject!, message: String) -> T {
-    let maybeResult: T? = value as? T
-    guard let result = maybeResult else {
-        fatalError(message)
-    }
-    
-    return result
-}
-
-public func castOrFatalError<T>(_ value: Any!) -> T {
-    let maybeResult: T? = value as? T
-    guard let result = maybeResult else {
-        fatalError("Failure converting from \(String(describing: value)) to \(T.self)")
-    }
-    
-    return result
-}
-
 infix operator <->
 
 public func <-><T: Equatable>(BehaviorRelay: BehaviorRelay<T>, property: ControlProperty<T>) -> Disposable {
@@ -109,8 +62,4 @@ public func <-><T: Equatable>(left: ControlProperty<T>, right: PublishSubject<T>
             .bind(to: left)
 
     return Disposables.create(leftToRight, rightToLeft)
-}
-
-public enum MustacheRxSwiftError: Error {
-    case deallocated
 }
