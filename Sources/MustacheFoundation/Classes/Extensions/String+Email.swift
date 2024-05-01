7395342d611aa9a496146f7c3673cc65e595f1e2
Tommy Hinrichsen <th@mustache.dk>
2024-05-01 10:08:37 +0200

    Updated internal function to check all matctes

--- Sources/MustacheFoundation/Classes/Extensions/String+Email.swift
+++ Sources/MustacheFoundation/Classes/Extensions/String+Email.swift
@@ -5,16 +5,26 @@ public extension String {
     
     /// Uses NSDataDetector to detect if string is valid email
     var isEmail: Bool {
-        let email = self.trimmingCharacters(in: .whitespacesAndNewlines)
-        let emailDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        let range = NSRange(email.endIndex..., in: email)
-        let matches = emailDetector?.matches(in: email, options: [], range: range)
-        guard matches?.count == 1, let match = matches?.first else { return false }
+        var email = self.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        guard match.range == range else { return false }
-        guard match.url?.scheme == "mailto" else { return false }
+        let types: NSTextCheckingResult.CheckingType = .link
         
-        return true
+        do {
+            let detector = try NSDataDetector(types: types.rawValue)
+            let matches = detector.matches(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count))
+            
+            for match in matches {
+                if match.resultType == .link,
+                   let url = match.url,
+                   url.scheme == "mailto",
+                   match.range.length == email.utf16.count { // Ensures the entire string is a valid email
+                    return true
+                }
+            }
+        } catch {
+            print("Invalid detector.")
+        }
+        return false
     }
     
 }
