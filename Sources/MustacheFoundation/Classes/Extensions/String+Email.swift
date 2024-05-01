5d62b8d713909dbc78c5aa1f228a247d50f9843e
Tommy Hinrichsen <th@mustache.dk>
2024-05-01 09:58:46 +0200

    Updated range constructor

--- Sources/MustacheFoundation/Classes/Extensions/String+Email.swift
+++ Sources/MustacheFoundation/Classes/Extensions/String+Email.swift
@@ -7,7 +7,7 @@ public extension String {
     var isEmail: Bool {
         let email = self.trimmingCharacters(in: .whitespacesAndNewlines)
         let emailDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        let range = NSRange(email.startIndex...email.endIndex, in: email)
+        let range = NSRange(email.endIndex..., in: email)
         let matches = emailDetector?.matches(in: email, options: [], range: range)
         guard matches?.count == 1, let match = matches?.first else { return false }
         
