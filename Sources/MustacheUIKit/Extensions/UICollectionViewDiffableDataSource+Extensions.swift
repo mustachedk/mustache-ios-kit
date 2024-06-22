ad76cdfc09d992fc3b418d8fe4fdd028dab4fcf0
Tommy Hinrichsen <th@mustache.dk>
2024-06-22 07:40:28 +0200

    Added convience for reapply snapshot to datasource

---
+++ Sources/MustacheUIKit/Extensions/UICollectionViewDiffableDataSource+Extensions.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+public extension UICollectionViewDiffableDataSource {
+    
+    /// Reapplies the current snapshot to the data source, animating the differences.
+    /// - Parameters:
+    ///   - completion: A closure to be called on completion of reapplying the snapshot.
+    func refresh(completion: (() -> Void)? = nil) {
+        self.apply(self.snapshot(), animatingDifferences: true, completion: completion)
+    }
+    
+    
+}
