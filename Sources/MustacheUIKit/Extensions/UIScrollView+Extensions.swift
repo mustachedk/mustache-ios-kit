73d7595735ec1b6dc4afc0ba7a7afd0c9b392eea
Tommy Hinrichsen <th@mustache.dk>
2024-07-01 21:18:43 +0200

    Added extra convience methods on scrollview

--- Sources/MustacheUIKit/Extensions/UIScrollView+Extensions.swift
+++ Sources/MustacheUIKit/Extensions/UIScrollView+Extensions.swift
@@ -9,6 +9,14 @@ public extension UIScrollView {
         return Int((self.contentOffset.x + (0.5 * self.frame.size.width)) / self.frame.width)
     }
     
+    var previousPage: Int {
+        return self.isScrolledToBegining ? self.currentPage : self.currentPage - 1
+    }
+    
+    var nextPage: Int {
+        return self.isScrolledToEnd ? self.currentPage : self.currentPage + 1
+    }
+    
     var scrollDirection: UICollectionView.ScrollDirection? {
         if self.contentSize.width > self.contentSize.height {
             return .horizontal
@@ -16,6 +24,32 @@ public extension UIScrollView {
             return .vertical
         } else {
             return nil
+        }
+    }
+    
+    var isScrolledToBegining: Bool {
+        
+        switch self.scrollDirection {
+            case .horizontal:
+                return self.contentOffset.x == 0
+            case .vertical:
+                return self.contentOffset.y == 0
+            default:
+                return self.contentOffset.x == 0 && self.contentOffset.y == 0
+        }
+    }
+    
+    var isScrolledToEnd: Bool {
+        
+        switch self.scrollDirection {
+            case .horizontal:
+                return self.contentOffset.x + self.frame.size.width >= self.contentSize.width
+            case .vertical:
+                return self.contentOffset.y + self.frame.size.height >= self.contentSize.height
+            default:
+                return self.contentOffset.x + self.frame.size.width >= self.contentSize.width &&
+                self.contentOffset.y + self.frame.size.height >= self.contentSize.height
+                
         }
     }
 }
