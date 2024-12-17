c63ad86ac659abfc9b1846cd689552c75a2c1581
Tommy Hinrichsen <th@mustache.dk>
2024-12-17 09:54:28 +0100

    Added completion to CoordinatorDelegate

--- Sources/MustacheServices/Coordinator/CoordinatorDelegate.swift
+++ Sources/MustacheServices/Coordinator/CoordinatorDelegate.swift
@@ -2,12 +2,16 @@ import Foundation
 
 public protocol CoordinatorDelegate: CoordinatorType {
     
-    func completed(child: CoordinatorType?)
+    func completed(child: CoordinatorType?, completion: Completion?)
     
 }
 
 public extension CoordinatorDelegate {
     
-    func completed(child: CoordinatorType?) {}
+    func completed(child: CoordinatorType?) {
+        self.completed(child: child, completion: nil)
+    }
+    
+    func completed(child: CoordinatorType?, completion: Completion?) {}
     
 }
