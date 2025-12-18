89ec911b97f430fedb45ffb6de06f2f0e3f699cc
Tommy Sadiq Hinrichsen <th@mustache.dk>
2025-12-18 09:31:05 +0100

    Out comment ATT to avoid rejection in review

--- Sources/MustacheUIKit/Extensions/UIViewController+ATT.swift
+++ Sources/MustacheUIKit/Extensions/UIViewController+ATT.swift
@@ -1,25 +1,26 @@
 
-#if canImport(UIKit)
-
-import Foundation
-import UIKit
-#if canImport(AppTrackingTransparency)
-import AppTrackingTransparency
-
-@available(iOS 14, *)
-public extension UIViewController {
-    
-    func requestTrackingAuthorization(_ completion: @escaping () -> Void) -> Bool {
-        guard ATTrackingManager.trackingAuthorizationStatus.rawValue != ATTrackingManager.AuthorizationStatus.notDetermined.rawValue else {
-            ATTrackingManager.requestTrackingAuthorization(completionHandler: { _ in
-                DispatchQueue.main.async(execute: { completion() })
-            })
-            return false
-        }
-        return true
-    }
-}
-
-#endif
-
-#endif
+// Manually import this into your project, if we are not using AppTrackingTransparency we still are forced to declare it if we have this code
+//#if canImport(UIKit)
+//
+//import Foundation
+//import UIKit
+//#if canImport(AppTrackingTransparency)
+//import AppTrackingTransparency
+//
+//@available(iOS 14, *)
+//public extension UIViewController {
+//    
+//    func requestTrackingAuthorization(_ completion: @escaping () -> Void) -> Bool {
+//        guard ATTrackingManager.trackingAuthorizationStatus.rawValue != ATTrackingManager.AuthorizationStatus.notDetermined.rawValue else {
+//            ATTrackingManager.requestTrackingAuthorization(completionHandler: { _ in
+//                DispatchQueue.main.async(execute: { completion() })
+//            })
+//            return false
+//        }
+//        return true
+//    }
+//}
+//
+//#endif
+//
+//#endif
